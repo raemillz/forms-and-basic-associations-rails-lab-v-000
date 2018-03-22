3df575ff2496aff91ab5298d64453633f59bf140
Rachel Miller <raeanne1991@gmail.com>
2018-03-22 20:52:24 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -47,7 +47,7 @@ class SongsController < ApplicationController
   private
 
   def song_params
-    params.require(:song).permit(:title, contents:[])
+    params.require(:song).permit(:title, :artist_name, contents:[])
   end
 end
 
